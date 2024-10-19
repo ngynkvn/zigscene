961b03e5cb810810e69c0748cd4452111f8d32bc
Kevin <ngynkvn@gmail.com>
2024-10-19 17:34:48 -0400

    3d circle

--- src/main.zig
+++ src/main.zig
@@ -5,13 +5,15 @@ const c = @cImport({
 });
 const audio = @import("audio.zig");
 const graphics = @import("graphics.zig");
-const APP_NAME = "zigscene";
+
 const screenWidth = 800;
 const screenHeight = 600;
+const APP_NAME = "zigscene";
 
 pub fn main() !void {
     var t: f32 = 0.0;
 
+    // Setup
     c.InitWindow(screenWidth, screenHeight, APP_NAME);
     defer c.CloseWindow(); // Close window and OpenGL context
 
@@ -21,7 +23,7 @@ pub fn main() !void {
     var music = c.Music{};
     var filename = std.mem.zeroes([64:0]u8);
     var clen: usize = 0;
-    var info = std.mem.zeroes([256:0]u8);
+    var txtbuffer = std.mem.zeroes([256:0]u8);
     c.SetMasterVolume(0.10);
 
     const camera3d: c.Camera3D = .{
@@ -31,11 +33,6 @@ pub fn main() !void {
         .fovy = 45.0, // Camera field-of-view Y
         .projection = c.CAMERA_PERSPECTIVE, // Camera projection type
     };
-    const camera2d: c.Camera2D = .{
-        .zoom = 1,
-        .offset = .{ .x = screenWidth / 2, .y = screenHeight / 2 },
-    };
-
     c.SetTargetFPS(60); // Set our game to run at 60 frames-per-second
 
     // Main game loop
@@ -54,23 +51,21 @@ pub fn main() !void {
             c.UpdateMusicStream(music);
         }
 
-        c.BeginMode2D(camera2d);
-        defer c.EndMode2D();
-
         c.BeginDrawing();
         defer c.EndDrawing();
+        const center = c.GetWorldToScreen(.{ .x = 0, .y = 0 }, camera3d);
+
         c.ClearBackground(c.BLACK);
         if (c.IsMusicStreamPlaying(music)) {
             const mtp = c.GetMusicTimePlayed(music);
             const mtl = c.GetMusicTimeLength(music);
-            const txt = try std.fmt.bufPrint(&info, "{s}\n{d:3.2} | {d:3.2}", .{ filename[0..clen], mtl, mtp });
+            const txt = try std.fmt.bufPrint(&txtbuffer, "{s}\n{d:3.2} | {d:3.2}", .{ filename[0..clen], mtl, mtp });
             c.DrawText(txt.ptr, 0, 0, 10, c.WHITE);
         }
-        const center = c.GetWorldToScreen(.{ .x = 0, .y = 0 }, camera3d);
         for (audio.curr_buffer[0..audio.curr_len], 0..) |v, i| {
             graphics.draw_line(center, i, v);
             graphics.draw_bars(center, i, v);
-            graphics.draw_bubbles(center, i, v, t);
+            //graphics.draw_bubbles(center, i, v, t);
         }
         for (audio.curr_fft[0..audio.curr_len], 0..) |v, i| {
             const SPACING = 6;
@@ -82,6 +77,31 @@ pub fn main() !void {
             c.DrawRectangleRec(.{ .x = px, .y = py, .width = 3, .height = 2 }, c.RAYWHITE);
             c.DrawRectangleRec(.{ .x = px, .y = py + 12, .width = 3, .height = y + 2 }, c.RED);
         }
+
+        {
+            c.BeginMode3D(camera3d);
+            defer c.EndMode3D();
+            c.DrawGrid(40.0, 2);
+            const R = 2;
+            const SCALE = 0.5;
+            const N_SLICES = 3;
+            c.rlPushMatrix();
+            c.rlRotatef(t * 100, 1, 1, 1);
+            const tsteps = std.math.pi * 2 / @as(f32, @floatFromInt(audio.curr_len));
+            for (0..N_SLICES) |j| {
+                c.rlPushMatrix();
+                c.rlRotatef(@as(f32, @floatFromInt(j)) * 60, 1, 0, 0);
+                for (audio.curr_buffer[0..audio.curr_len], 0..) |v, i| {
+                    const r = R + (@abs(v) * SCALE);
+                    const x = (@cos(@as(f32, @floatFromInt(i)) * tsteps + t) * r);
+                    const y = (@sin(@as(f32, @floatFromInt(i)) * tsteps + t) * r);
+                    c.DrawCubeWires(.{ .x = x, .y = y, .z = 0 }, 0.05, 0.05, 0.05, c.ORANGE);
+                }
+                c.rlPopMatrix();
+            }
+            c.rlPopMatrix();
+        }
+
         t += 0.01;
     }
 }
