7058baf96e3064101b1dc7e99ae89a5a98bf1dad
Kevin <ngynkvn@gmail.com>
2024-10-27 14:11:02 -0400

    feat: color picker

--- src/gui/color_picker.zig
+++ src/gui/color_picker.zig
@@ -1,94 +1,85 @@
 const std = @import("std");
 const rl = @import("../raylib.zig");
 const cnv = @import("../ext/convert.zig");
-const iff = cnv.iff;
+
+const BASE_COLOR_DISABLED = rl.BASE_COLOR_DISABLED;
+//const BORDER = rl.BORDER;
+const BORDER = 0;
+const BORDER_COLOR_DISABLED = rl.BORDER_COLOR_DISABLED;
+const BORDER_WIDTH = rl.BORDER_WIDTH;
+const COLORPICKER = rl.COLORPICKER;
+const HUEBAR_SELECTOR_HEIGHT = rl.HUEBAR_SELECTOR_HEIGHT;
+const HUEBAR_SELECTOR_OVERFLOW = rl.HUEBAR_SELECTOR_OVERFLOW;
+const MOUSE_BUTTON_LEFT = rl.MOUSE_BUTTON_LEFT;
+const STATE_DISABLED = rl.STATE_DISABLED;
+const STATE_FOCUSED = rl.STATE_FOCUSED;
+const STATE_PRESSED = rl.STATE_PRESSED;
+// const guiAlpha = rl.guiAlpha;
+const guiAlpha = 1.0;
+var guiControlExclusiveMode = false;
+// const guiControlExclusiveMode = rl.guiControlExclusiveMode;
+pub var guiControlExclusiveRec: Rectangle = Rectangle{ .x = 0, .y = 0, .width = 0, .height = 0 };
+// const guiControlExclusiveRec = rl.guiControlExclusiveRec;
+
 const ffi = cnv.ffi;
-const Rectangle = rl.Rectangle;
-const GuiState = rl.GuiState;
+const iff = cnv.iff;
+const ceilf = std.math.ceil;
+
+const CheckCollisionPointRec = rl.CheckCollisionPointRec;
+const DrawRectangle = rl.DrawRectangle;
+const DrawRectangleGradientH = rl.DrawRectangleGradientH;
+const Fade = rl.Fade;
+const GetColor = rl.GetColor;
+const GetMousePosition = rl.GetMousePosition;
 const GuiGetState = rl.GuiGetState;
 const GuiGetStyle = rl.GuiGetStyle;
 const GuiIsLocked = rl.GuiIsLocked;
-const COLORPICKER: c_int = rl.COLORPICKER;
-const HUEBAR_SELECTOR_HEIGHT: c_int = rl.HUEBAR_SELECTOR_HEIGHT;
-const HUEBAR_SELECTOR_OVERFLOW: c_int = rl.HUEBAR_SELECTOR_OVERFLOW;
-const STATE_DISABLED: c_int = rl.STATE_DISABLED;
-const GetMousePosition = rl.GetMousePosition;
-const Vector2 = rl.Vector2;
-// const guiControlExclusiveMode = rl.guiControlExclusiveMode;
-var guiControlExclusiveMode = false;
 const IsMouseButtonDown = rl.IsMouseButtonDown;
-const MOUSE_BUTTON_LEFT = rl.MOUSE_BUTTON_LEFT;
-//const guiControlExclusiveRec = rl.guiControlExclusiveRec;
-pub var guiControlExclusiveRec: Rectangle = Rectangle{
-    .x = 0,
-    .y = 0,
-    .width = 0,
-    .height = 0,
-};
-const STATE_PRESSED = rl.STATE_PRESSED;
-const CheckCollisionPointRec = rl.CheckCollisionPointRec;
-const STATE_FOCUSED = rl.STATE_FOCUSED;
-const DrawRectangleGradientH = rl.DrawRectangleGradientH;
-const DrawRectangle = rl.DrawRectangle;
-const ceilf = std.math.ceil;
-const Fade = rl.Fade;
+
 const Color = rl.Color;
-// const guiAlpha = rl.guiAlpha;
-const guiAlpha = 1.0;
-const GetColor = rl.GetColor;
-// const GuiFade = rl.GuiFade;
-const BASE_COLOR_DISABLED = rl.BASE_COLOR_DISABLED;
-const BORDER_COLOR_DISABLED = rl.BORDER_COLOR_DISABLED;
-const BORDER_WIDTH = rl.BORDER_WIDTH;
-// const BORDER= rl.BORDER;
-const BORDER: c_int = 0;
+const GuiState = rl.GuiState;
+const Rectangle = rl.Rectangle;
+const Vector2 = rl.Vector2;
 
 pub fn GuiColorBarHueH(bounds: Rectangle, text: [*c]const u8, hue: [*c]f32) c_int {
-    _ = text; // autofix
-    const result: c_int = 0;
-    var state: c_uint = @intCast(GuiGetState());
+    _ = text; // TODO: Draw text
+    var state: c_int = GuiGetState();
     const selector: Rectangle = Rectangle{
-        .x = (bounds.x + ((hue.* / 360.0) * bounds.width)) - @as(f32, @floatFromInt(@divTrunc(GuiGetStyle(COLORPICKER, HUEBAR_SELECTOR_HEIGHT), 2))),
-        .y = bounds.y - @as(f32, @floatFromInt(GuiGetStyle(COLORPICKER, HUEBAR_SELECTOR_OVERFLOW))),
-        .width = @as(f32, @floatFromInt(GuiGetStyle(COLORPICKER, HUEBAR_SELECTOR_HEIGHT))),
-        .height = bounds.height + @as(f32, @floatFromInt(GuiGetStyle(COLORPICKER, HUEBAR_SELECTOR_OVERFLOW) * 2)),
+        .x = (bounds.x + ((hue.* / 360.0) * bounds.width)) - ffi(f32, @divTrunc(GuiGetStyle(COLORPICKER, HUEBAR_SELECTOR_HEIGHT), 2)),
+        .y = bounds.y - ffi(f32, GuiGetStyle(COLORPICKER, HUEBAR_SELECTOR_OVERFLOW)),
+        .width = ffi(f32, GuiGetStyle(COLORPICKER, HUEBAR_SELECTOR_HEIGHT)),
+        .height = bounds.height + ffi(f32, GuiGetStyle(COLORPICKER, HUEBAR_SELECTOR_OVERFLOW) * 2),
     };
-    if ((state != @as(c_uint, @bitCast(STATE_DISABLED))) and !GuiIsLocked()) {
+    if (state != STATE_DISABLED and !GuiIsLocked()) {
         const mousePoint: Vector2 = GetMousePosition();
         if (guiControlExclusiveMode) {
             if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-                if ((((bounds.x == guiControlExclusiveRec.x) and (bounds.y == guiControlExclusiveRec.y)) and (bounds.width == guiControlExclusiveRec.width)) and (bounds.height == guiControlExclusiveRec.height)) {
-                    state = @as(c_uint, @bitCast(STATE_PRESSED));
+                if (bounds.x == guiControlExclusiveRec.x and
+                    bounds.y == guiControlExclusiveRec.y and
+                    bounds.width == guiControlExclusiveRec.width and
+                    bounds.height == guiControlExclusiveRec.height)
+                {
+                    state = STATE_PRESSED;
                     hue.* = ((mousePoint.x - bounds.x) * 360) / bounds.width;
                     hue.* = std.math.clamp(hue.*, 0, 359);
                 }
             } else {
                 guiControlExclusiveMode = false;
-                guiControlExclusiveRec = Rectangle{
-                    .x = 0,
-                    .y = 0,
-                    .width = 0,
-                    .height = 0,
-                };
+                guiControlExclusiveRec = Rectangle{ .x = 0, .y = 0, .width = 0, .height = 0 };
             }
-        } else if ((@as(c_int, @intFromBool(CheckCollisionPointRec(mousePoint, bounds))) != 0) or (@as(c_int, @intFromBool(CheckCollisionPointRec(mousePoint, selector))) != 0)) {
+        } else if (CheckCollisionPointRec(mousePoint, bounds) or CheckCollisionPointRec(mousePoint, selector)) {
             if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-                state = @as(c_uint, @bitCast(STATE_PRESSED));
-                guiControlExclusiveMode = 1 != 0;
+                state = STATE_PRESSED;
+                guiControlExclusiveMode = true;
                 guiControlExclusiveRec = bounds;
                 hue.* = ((mousePoint.x - bounds.x) * 360) / bounds.width;
-                if (hue.* <= 0.0) {
-                    hue.* = 0.0;
-                }
-                if (hue.* >= 359.0) {
-                    hue.* = 359.0;
-                }
+                hue.* = std.math.clamp(hue.*, 0, 359);
             } else {
-                state = @as(c_uint, @bitCast(STATE_FOCUSED));
+                state = STATE_FOCUSED;
             }
         }
     }
-    if (state != @as(c_uint, @bitCast(STATE_DISABLED))) {
+    if (state != STATE_DISABLED) {
         DrawRectangleGradientH(
             iff(c_int, bounds.x),
             iff(c_int, bounds.y),
@@ -159,7 +150,7 @@ pub fn GuiColorBarHueH(bounds: Rectangle, text: [*c]const u8, hue: [*c]f32) c_in
         Color{ .r = 0, .g = 0, .b = 0, .a = 0 },
         GetColor(@bitCast(GuiGetStyle(COLORPICKER, BORDER + @as(c_int, @bitCast(state)) * 3))),
     );
-    return result;
+    return 0;
 }
 pub fn GuiDrawRectangle(rec: Rectangle, borderWidth: c_int, borderColor: Color, color: Color) callconv(.c) void {
     if (color.a > 0) {
@@ -202,6 +193,8 @@ pub fn GuiDrawRectangle(rec: Rectangle, borderWidth: c_int, borderColor: Color, 
         );
     }
 }
+
+// const GuiFade = rl.GuiFade;
 pub fn GuiFade(color: Color, alpha: f32) callconv(.c) Color {
     const a = std.math.clamp(alpha, 0, 1);
     return Color{ .r = color.r, .g = color.g, .b = color.b, .a = iff(u8, ffi(f32, color.a) * a) };
