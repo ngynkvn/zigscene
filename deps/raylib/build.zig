f33f0190e43826e7da0396ad6d36c7974e0333a9
Kevin <ngynkvn@gmail.com>
2024-10-26 21:13:44 -0400

    refactor: Clean up building raylib

--- deps/raylib/build.zig
+++ deps/raylib/build.zig
@@ -8,6 +8,8 @@ pub fn build(b: *std.Build) !void {
         .optimize = optimize,
     });
     const libraylib = raylib.artifact("raylib");
+    libraylib.step.name = "Compile Raylib";
+
     b.installArtifact(libraylib);
     libraylib.root_module.addCMacro("SUPPORT_FILEFORMAT_FLAC", "1");
     // SOURCE: https://github.com/Not-Nik/raylib-zig/blob/c191e12e7c50e5dc2b1addd1e5dbd16bd405d2b5/build.zig#L119
@@ -16,41 +18,49 @@ pub fn build(b: *std.Build) !void {
         .target = target,
         .optimize = optimize,
     });
-    const libraygui = b.addStaticLibrary(.{
-        .name = "raygui",
-        .target = target,
-        .optimize = optimize,
-    });
-    libraygui.addIncludePath(raylib.path("src"));
-    libraygui.defineCMacro("RAYGUI_IMPLEMENTATION", "1");
-    libraygui.addCSourceFiles(.{
-        .root = raygui.path("."),
-        .files = &.{"src/raygui.h"},
-    });
-    b.installArtifact(libraygui);
-
-    var raylib_c = b.addWriteFile("raylib.c",
-        \\#include "raylib.h"
-        \\#include "raymath.h"
-        \\#include "rlgl.h"
+    // Lest some other poor soul stumble upon here...
+    // You CANNOT link and reference static functions apparently.
+    // Create a wrapper function instead calling the static function
+    // I wasted so much time on this :|
+    var raygui_c = b.addWriteFile("raygui.c",
         \\#define RAYGUI_IMPLEMENTATION
         \\#include "raygui.h"
         \\#include "style_dark.h"
+        \\void RayguiDark(void){ GuiLoadStyleDark(); };
     );
-    _ = raylib_c.addCopyDirectory(raylib.path("src"), ".", .{ .include_extensions = &.{"h"} });
-    _ = raylib_c.addCopyFile(raygui.path("src/raygui.h"), "raygui.h");
-    _ = raylib_c.addCopyFile(raygui.path("styles/dark/style_dark.h"), "style_dark.h");
+    raygui_c.step.name = "Generate raygui implementation";
 
+    libraylib.installHeader(raygui.path("src/raygui.h"), "raygui.h");
+    libraylib.addCSourceFile(.{
+        .file = raygui_c.getDirectory().path(b, "raygui.c"),
+    });
+    libraylib.addIncludePath(raylib.path("src"));
+    libraylib.addIncludePath(raygui.path("src"));
+    libraylib.addIncludePath(raygui.path("styles/dark"));
+
+    b.installArtifact(libraylib);
+
+    const file = b.addWriteFile("raylib.gen.c",
+        \\#include "raylib.h"
+        \\#include "raymath.h"
+        \\#include "rlgl.h"
+        \\#include "raygui.h"
+        \\void RayguiDark(void);
+    );
+    file.step.name = "Generate c file for zig translation";
     const translate_c = b.addTranslateC(.{
-        .root_source_file = raylib_c.getDirectory().path(b, "raylib.c"),
+        .root_source_file = file.getDirectory().path(b, "raylib.gen.c"),
         .target = b.graph.host,
         .optimize = optimize,
         .link_libc = true,
     });
+    translate_c.step.name = "Perform translate-c";
+    translate_c.addIncludePath(raylib.path("src"));
     translate_c.addIncludePath(raygui.path("src"));
+    translate_c.addIncludePath(raygui.path("styles/dark"));
+
     const module = translate_c.addModule("raylib");
     module.linkLibrary(libraylib);
-    b.getInstallStep().dependOn(&b.addInstallFile(translate_c.getOutput(), "c.zig").step);
 
     const check_step = b.step("check", "check build");
     check_step.dependOn(&libraylib.step);
--- src/main.zig
+++ src/main.zig
@@ -30,7 +30,7 @@ pub fn main() !void {
     defer rl.CloseAudioDevice();
 
     rl.GuiSetAlpha(0.8);
-    rl.GuiLoadStyleDark();
+    rl.RayguiDark();
     //try music.startMusic("./sounds/willix.mp3");
 
     var rot_offset: f32 = 0.0;
