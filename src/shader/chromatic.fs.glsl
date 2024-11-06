e4bae2513e34ea7b14841634baae9009fc339aa9
Kevin <ngynkvn@gmail.com>
2024-11-06 00:44:46 -0500

    jfc i just destroyed my eardrums

--- src/shader/chromatic.fs.glsl
+++ src/shader/chromatic.fs.glsl
@@ -1,30 +1,32 @@
-#version 330
+#version 100
+
+// Precision qualifiers required for GLSL ES
+precision mediump float;
 
 // Input vertex attributes (from vertex shader)
-in vec2 fragTexCoord;
-in vec4 fragColor;
+varying vec2 fragTexCoord;
+varying vec4 fragColor;
 
 // Input uniform values
 uniform sampler2D texture0;
 uniform vec4 colDiffuse;
 
-// Output fragment color
-out vec4 finalColor;
-
 // NOTE: Add here your custom variables
 uniform float chromaFactor;
 uniform float noiseFactor;
 
+// Helper function to generate pseudo-random noise
+float rand(vec2 co) {
+    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
+}
+
 void main()
 {
-    vec4 r = texture(texture0, vec2(fragTexCoord.x + chromaFactor, fragTexCoord.y));
-    vec4 g = texture(texture0, fragTexCoord);
-    vec4 b = texture(texture0, vec2(fragTexCoord.x - chromaFactor, fragTexCoord.y));
+    vec4 r = texture2D(texture0, vec2(fragTexCoord.x + chromaFactor, fragTexCoord.y));
+    vec4 g = texture2D(texture0, fragTexCoord);
+    vec4 b = texture2D(texture0, vec2(fragTexCoord.x - chromaFactor, fragTexCoord.y));
 
-    float noise = fract(sin(dot(fragTexCoord, vec2(12.9898, 78.233))) * 43758.5453);
-    // final color is the color from the texture 
-    //    times the tint color (colDiffuse)
-    //    times the fragment color (interpolated vertex color)
+    float noise = rand(fragTexCoord);
     vec4 chroma = vec4(r.r, g.g, b.b, 1.0) + vec4(noise * noiseFactor);
-    finalColor = colDiffuse*chroma;
+    gl_FragColor = colDiffuse * chroma;
 }
